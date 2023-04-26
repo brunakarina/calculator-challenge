e9f006f88d15e5e0c15daa018b763986db48403b
Bruna <brubskarina@gmail.com>
2023-04-26 17:42:14 -0300

    initial commit

---
+++ lib/calculadora.rb
@@ -0,0 +1,19 @@
+def soma(primeiro_numero, segundo_numero)
+	return primeiro_numero + segundo_numero
+end
+
+def subtracao(primeiro_numero, segundo_numero)
+  return primeiro_numero - segundo_numero
+end
+
+def multiplicacao(primeiro_numero, segundo_numero)
+  return primeiro_numero * segundo_numero
+end
+
+def divisao(x, y)
+  if y == 0
+    return 'Opa! Zero como divisor'
+  else
+    return x/y
+  end
+end
